4eab39ee34ff238e4deca213f3ea17f22f8cac2d
Veen <24ucb138praveen@kgkite.ac.in>
2026-01-28 20:27:54 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+echo "Enter Principal Amount:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time Period (in years):"
+read time
+
+# Calculate Simple Interest
+simple_interest=$(echo "($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest is: $simple_interest"
 
